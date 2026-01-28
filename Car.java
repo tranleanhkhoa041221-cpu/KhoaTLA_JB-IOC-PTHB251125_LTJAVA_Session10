29738bd56a525a36fd611b7a9893387b22b4edb5
tranleanhkhoa041221-cpu <tranleanhkhoa041221@gmail.com>
2026-01-28 16:58:08 +0700

    UPLOAD BTVNSESISON10_GIOI2

--- Car.java
+++ Car.java
@@ -1,14 +1,37 @@
-package BaiTapXuatSac2;
+package BaiTapGioi2;
 
-public class Car extends MotorVehicle{
-        @Override
-        public void startEngine() {
-            System.out.println(super.getBrand()+" (Car) engine starts: Vroom Vroom!");
+public class Car {
+        private int currentSpeed = 0;
+
+        public void accelerate() {
+            currentSpeed += 10;
+            System.out.println("Car accelerates by default: +10 km/h");
         }
+        public void accelerate(int speed) {
+            currentSpeed += speed;
+            System.out.println("Car accelerates by " + speed + " km/h");
+        }
+        public void accelerate(int speed, int seconds) {
+            int increase = speed * seconds;
+            currentSpeed += increase;
+            System.out.println("Car accelerates " + increase + " km/h (speed x time)");
+        }
+        public void printStatus() {
+            System.out.println("Current speed: " + currentSpeed + " km/h");
+        }
+        public static void main(String[] args) {
+            Car myCar = new Car();
+            myCar.accelerate();
+            myCar.printStatus();
 
-        public void openTrunk(){
-            System.out.println(super.getBrand()+" trunk is opening...");
+            myCar.accelerate(20);
+            myCar.printStatus();
+
+            myCar.accelerate(20, 1);
+            myCar.printStatus();
         }
     }
+
+
 
 
